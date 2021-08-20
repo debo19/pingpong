d2ec0fa75dafed4ef609b5898ac3bcf41e7b040e
Debopriya Bhattacharjee <debopriyabh1908@gmail.com>
2021-08-20 22:23:30 +0530

    changes

--- scorekeeper.js
+++ scorekeeper.js
@@ -1,9 +1,3 @@
-// const p1button=document.querySelector('#p1button');
-// const p2button=document.querySelector('#p2button');
-// const p1disp=document.querySelector('#p1disp');
-// const p2disp=document.querySelector('#p2disp');
-// let p1score=0;
-// let p2score=0;
 const p1={
     button: document.querySelector('#p1button'),
     disp: document.querySelector('#p1disp'),
@@ -23,11 +17,11 @@ let winningScore=3;
 let isGameOver=false;
 
 select.addEventListener('change',function(){
-    winningScore=parseInt(this.value);               //"this" refers to whatever option is selected in select playTo
+    winningScore=parseInt(this.value);               
     resetting();
 })
 
-function updateScores(player,opponent){           //refactoring to avoid repetition of code 
+function updateScores(player,opponent){           
     player.score+=1;
     if(player.score===winningScore){
         isGameOver=true;
@@ -43,7 +37,7 @@ p1.button.addEventListener('click',function(){
     if(isGameOver!==true){
         updateScores(p1,p2);  
     } 
-})                                              //if either one reaches winning score first, isGameOver is true and neither code runs it just stops game further 
+})                                              
 p2.button.addEventListener('click',function(){
     if(isGameOver!==true){
     updateScores(p2,p1);
@@ -52,8 +46,8 @@ p2.button.addEventListener('click',function(){
 resetButton.addEventListener('click',resetting);
 
 function resetting(){
-    isGameOver=false;               //back to initial condition
-    for(let p of [p1,p2]){          //to avoid repetition of code and also easier if multiple users are required
+    isGameOver=false;              
+    for(let p of [p1,p2]){         
         p.score=0;
         p.disp.textContent=0;
         p.button.disabled=false;
